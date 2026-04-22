6a6a5c1056abf98449cf04f71a0423f6f3d6c781
Oier35-prog <oier.aguadoopendeusto.es>
2026-04-22 16:39:49 +0200

    Craecion Tablas Activos y Intervencion

--- Activos/models.py
+++ Activos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Activo (models.Model):
+    Codigo_de_activacion = models.BooleanField(default=False)
+    Nombre = models.CharField(max_length=100)
+    Tipo_de_dispositivo = models.CharField(max_length=200)
+    Modelo = models.CharField(max_length=200)
+    Marca = models.CharField(max_length=100)
+    Fecha_instalacion = models.DateField(auto_now_add=True)
+    Ubicacion = models.CharField(max_length=200)
+    Estado_operativo = models.BooleanField(default=False)
--- Incidencias/models.py
+++ Incidencias/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from Tecnico.models import Trabajador
+from Activos.models import Activo
 
-# Create your models here.
+
+class Intervencion(models.Model):
+    Codigo = models.IntegerField()
+    Fecha_de_apertura = models.DateField(auto_created=True)
+    Fecha_de_cierra = models.DateField(auto_created=False)
+    Tipo_de_intervencion = models.CharField(max_length=250)
+    Descripcion = models.CharField(max_length=250)
+    Tecnico_asignado = models.ForeignKey(Trabajador, on_delete=models.CASCADE)
+    Activo_asignado = models.ForeignKey(Activo, on_delete=models.CASCADE)
