5a9d9f81e27882b25e4c2c60c13beb5a9daf1b76
josumartinezdezuazo <josu.martinezdezuazo@opendeusto.es>
2026-04-22 16:29:38 +0200

    Creacion de clase de la aplicacion Tecnico

--- Tecnico/models.py
+++ Tecnico/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Trabajador (models.Model):
+    DNI = models.CharField(max_length=9)
+    Nombre = models.CharField(max_length=30)
+    Apellido = models.CharField(max_length=40)
+    Email = models.EmailField(unique = true)
+    Telefono = models.CharField(max_length=15)
