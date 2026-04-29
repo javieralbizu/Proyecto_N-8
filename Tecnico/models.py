e290043b9b7c3ee37b455bf6b858ec71022a4676
Markel2004 <markel.ciruela@opendeusto.es>
2026-04-29 17:27:22 +0200

    Finalizado modelo Tecnico con método __str__

--- Tecnico/models.py
+++ Tecnico/models.py
@@ -6,3 +6,6 @@ class Trabajador (models.Model):
     Apellido = models.CharField(max_length=40)
     Email = models.EmailField()
     Telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.Nombre} {self.Apellido}"
