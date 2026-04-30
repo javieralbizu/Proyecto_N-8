38e06b9b6ac7ee7799af5414b86769eaa4d037dc
Markel2004 <markel.ciruela@opendeusto.es>
2026-04-30 16:32:05 +0200

    Formulario realizado Tecnico

--- Tecnico/urls.py
+++ Tecnico/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('Tecnico/',views.CargarTabla)
+    path('',views.CargarTabla, name='CargarTabla'),
+    path('nuevo/',views.nuevo_trabajador, name='nuevo_trabajador')
 ]
--- Tecnico/views.py
+++ Tecnico/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from .forms import TrabajadorForm
+from django.shortcuts import render, redirect
+from .models import Trabajador
 
 def CargarTabla(request):
-    return render(request, 'Tecnico/Tabla.html')
+    tecnicos = Trabajador.objects.all()
+    return render(request, 'Tecnico/Tabla.html', {'lista': tecnicos})
+
+def nuevo_trabajador(request):
+    if request.method == 'POST':
+        form = TrabajadorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('CargarTabla')
+    else:
+        form = TrabajadorForm()
+
+    return render(request, 'Tecnico/nuevo_trabajador.html', {'form': form})
 # Create your views here.
