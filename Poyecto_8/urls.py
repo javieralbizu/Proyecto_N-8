1e92ee7f4910575b26f7e7a1284e2fe28c7c02dc
josumartinezdezuazo <josu.martinezdezuazo@opendeusto.es>
2026-04-30 15:36:57 +0200

    HTML de Tecnico

--- Poyecto_8/urls.py
+++ Poyecto_8/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("Activos.urls")),
     path("", include("Incidencias.urls")),
+    path("", include("Tecnico.urls")),
 ]
--- Tecnico/views.py
+++ Tecnico/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
+def CargarTabla(request):
+    return render(request, 'Tecnico/Tabla.html')
 # Create your views here.
