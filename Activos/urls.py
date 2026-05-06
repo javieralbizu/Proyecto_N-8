19844aeb40995c329e937261bd7c004a18684ee2
Oier35-prog <oier.aguadoopendeusto.es>
2026-05-06 15:42:33 +0200

    Formulario de Activos sin errores

--- Activos/urls.py
+++ Activos/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('Activos/',views.CargarTabla)
+    path('Activos/',views.CargarTabla),
+    path('Activos/NuevoActivo/',views.NuevoActivo)
 ]
