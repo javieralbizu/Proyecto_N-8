38df379f643372ff2bb8dc7364f60504db97c6b9
javieralbizu <javier.albizu@opendeusto.es>
2026-05-06 20:15:41 +0200

    Eliminar y Editar Activos funcionando

--- Activos/urls.py
+++ Activos/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('Activos/',views.CargarTabla),
-    path('Activos/NuevoActivo/',views.NuevoActivo)
+    path('Activos/',views.TablaActivos, name='TablaActivos'),
+    path('Activos/NuevoActivo/',views.NuevoActivo, name='NuevoActivo'),
+    path('Activos/NuevoActivo/<int:id>/',views.NuevoActivo, name='EditarActivo'),
+    path('Activos/EliminarActivo/<int:id>/',views.EliminarActivo, name='EliminarActivo')
 ]
--- Activos/views.py
+++ Activos/views.py
@@ -1,18 +1,30 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Activo
 from .forms import ActivoForm
 
-def CargarTabla(request):
-    return render(request, 'Activos/Tabla.html')
+def TablaActivos(request):
+    Activos = Activo.objects.all()
+    return render(request, 'Activos/Tabla.html', {'Activos': Activos})
 
-def NuevoActivo(request):
-    if request.method == 'POST':
-        form = ActivoForm(request.POST)
+
+def NuevoActivo(request, id=None):
+    if id:
+        activo = get_object_or_404(Activo, id=id)
+    else:
+        activo = None
+
+    if request.method == "POST":
+        form = ActivoForm(request.POST, instance=activo)
         if form.is_valid():
             form.save()
-            return redirect('CargarTabla')
-        else:
-            return render(request, 'Activos/NuevoActivo.html', {'form': form})
-    else:  
-        return render(request, 'Activos/NuevoActivo.html', {'form': ActivoForm()})
-    
-# Create your views here.
+            return redirect("TablaActivos")
+    else:
+        form = ActivoForm(instance=activo)
+    return render(request, "Activos/NuevoActivo.html", {"form": form})
+
+def EliminarActivo(request, id):
+    activo = get_object_or_404(Activo, id = id)
+    activo.delete()
+    return redirect('TablaActivos')
+
+
