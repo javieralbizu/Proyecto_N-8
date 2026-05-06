a651e74bab487011733d3eeca844b2bbfa1805ec
javieralbizu <javier.albizu@opendeusto.es>
2026-05-06 20:43:07 +0200

    Editar y eliminar Tecnico funcionando

--- Tecnico/urls.py
+++ Tecnico/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('Tecnico/',views.CargarTabla, name='CargarTabla'),
-    path('Tecnico/Nuevo/',views.NuevoTrabajador, name='NuevoTrabajador')
+    path('Tecnico/Nuevo/',views.NuevoTrabajador, name='NuevoTrabajador'),
+    path('Tecnico/Nuevo/<int:id>',views.NuevoTrabajador, name='EditarTrabajador'),
+    path('Tecnico/EliminarTrabajador/<int:id>',views.EliminarTecnico, name='EliminarTrabajador')
 ]
